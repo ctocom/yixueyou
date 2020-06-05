c578cf3eceeffc703738ae7bbd7fb8d0fe5579c3
yueluchuan <166660614@qq.com>
2020-06-05 14:54:47 +0800

    ok

--- application/index/controller/StudentCourse.php
+++ application/index/controller/StudentCourse.php
@@ -5,6 +5,7 @@ namespace app\index\controller;
 
 use think\Controller;
 use app\index\model\Course;
+use app\index\model\Section;
 class StudentCourse extends Controller
 {
     //课程数据
@@ -17,7 +18,7 @@ class StudentCourse extends Controller
     public function section()
     {
         $course_id=$this->request->post('course_id');
-        $section_info=Course::where(['is_show'=>1,'delete_time'=>0,'course_id'=>$course_id])->select();
+        $section_info=Section::where(['is_show'=>1,'delete_time'=>0,'course_id'=>$course_id])->select();
         show($section_info,200,'ok');
     }
 }
