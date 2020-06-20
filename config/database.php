8dcd764699acc21c8b700db0109082d8d3092a23
yueluchuan <166660614@qq.com>
2020-06-20 16:49:14 +0800

    a

--- config/database.php
+++ config/database.php
@@ -17,7 +17,7 @@ return [
     // 数据库名
     'database'        => 'api_yixueyouke_n',
     // 用户名
-    'username'        => 'api_yixueyouke_n',
+    'username'        => 'root',
     // 密码
     'password'        => '5SJGHhkKL57w4Z4A',
     // 端口
